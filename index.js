14eface4e2a55839cd9a38221bb76feeb781e7cd
Yan Hankin <hankinyb@gmail.com>
2018-01-11 05:09:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ var uppercase = "HELLO!"
 var lowercase = "hello!"
 var mixedCase = "Hi there!"
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase)
+  if (string === string.toUpperCase())
   return 'YES INDEED!'
-  else if (string === string.toLowerCase)
+  else if (string === string.toLowerCase())
   return 'I can\'t hear you!'
   else if (string === "I love you, Grandma.")
   return 'I love you, too.'
